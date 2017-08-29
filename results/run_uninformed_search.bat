75c1ece5a954bdf506e086bea268b6bbbb625e54
Rohit Mehra <contactrohitmehra@gmail.com>
2017-08-29 15:35:55 -0500

    first commit

---
+++ results/run_uninformed_search.bat
@@ -0,0 +1,4 @@
+python ../run_search.py -p 1 -s 1 2 3 4 5 6 7 >> run_uninformed_search_results_p1.txt
+python ../run_search.py -p 2 -s 1 2 3 4 5 6 7 >> run_uninformed_search_results_p2.txt
+
+python ../run_search.py -p 3 -s 1 2 3 4 5 6 7 >> run_uninformed_search_results_p3.txt
